20f37559e6207ae59087fbef0424d0023d2951df
octavianmetta <octavian.wardhana@indosystem.com>
2019-07-15 11:15:10 +0700

    Initial Commit

---
+++ Bullseye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Octavian Wisnu on 7/15/1 R.
+//  Copyright © 1 Octavian Metta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
