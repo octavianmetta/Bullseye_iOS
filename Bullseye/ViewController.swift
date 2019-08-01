fd41116a37c54d05e9e786c1f467e60a708a0ff4
octavianmetta <octavian.wardhana@indosystem.com>
2019-08-01 15:01:07 +0700

    optimize code

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -11,38 +11,81 @@ import Foundation
 
 class ViewController: UIViewController {
     
-    var counter = 0
     var randomNumber = 0
+    var currentValue = 0
+    var round = 0
+    var score = 0
 
     @IBOutlet weak var numLabel: UILabel!
     @IBOutlet weak var numSlider: UISlider!
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        randomNumber = Int(arc4random_uniform(101))
-        numLabel.text = "Move the slider to : \(randomNumber)"
+       startNewRound()
+    }
+    
+
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        currentValue = Int(numSlider.value.rounded())
     }
     
     @IBAction func checkValue(_ sender: Any) {
+        var title: String
+        var message: String
+        let differences = abs(currentValue - randomNumber)
         if (Int(numSlider.value) < randomNumber) {
-            resultLabel.text = "You're too left  \(Int(numSlider.value) - randomNumber)"
-            resultLabel.backgroundColor = UIColor.red
+            title = "Whoops"
+            message = "You're too left \(differences)"
         }
         else if (Int(numSlider.value) > randomNumber){
-            resultLabel.text = "You're too right \(randomNumber - Int(numSlider.value))"
-            resultLabel.backgroundColor = UIColor.red
+            title = "Whoops"
+            message = "You're too right \(differences)"
         }
         else {
-            resultLabel.text = "Mata kebo"
-            resultLabel.backgroundColor = UIColor.green
-            let alert = UIAlertController(title: "Congrats", message: "Mata kebo", preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(action)
+            title = "Mata kebo"
+            message = "You did it"
             
-            present(alert, animated: true, completion:  nil)
         }
+        score = score + (100 - differences)
+        scoreLabel.text = "\(score)"
+        showAlertDialog(title, message)
+        startNewRound()
+    }
+    
+    @IBAction func playAgain(_ sender: Any) {
+        playAgain()
+    }
+    
+    fileprivate func showAlertDialog(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion:  nil)
+    }
+    
+    func startNewRound() {
+        randomNumber = Int.random(in: 1...100)
+        round += 1
+        updateLabel()
+    }
+    
+    func playAgain(){
+        numSlider.value = 50
+        randomNumber = Int.random(in: 1...100)
+        round = 0
+        score = 0
+        updateLabel()
+    }
+    
+    func updateLabel(){
+        numLabel.text = "Move the slider to : \(randomNumber)"
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
